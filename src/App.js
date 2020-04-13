3efaead660ea9077eb6e375be10b2d258e729be6
Luke Jones <11872906+joker-jonesy@users.noreply.github.com>
2020-04-13 13:10:25 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
 import React from 'react';
 import './App.css';
 
-
-
-
-function App(props) {
+function App() {
 
   const [position, setPosition] = React.useState({});
-  const [error, setError] = React.useState(null);
+  const [error, setError]  =React.useState(null);
+  const [color, setColor]=React.useState("red");
 
-  const onChange = ({coords}) => {
+  const onChange = ({coords})=>{
     setPosition({
-      latitude: coords.latitude,
-      longitude: coords.longitude,
-    });
+      x:coords.latitude,
+      y:coords.longitude,
+      z:coords.altitude,
+      accuracy: coords.accuracy
+    })
   };
-  const onError = (error) => {
+
+  const onError = (error)=>{
     setError(error.message);
   };
-  React.useEffect(() => {
+
+  React.useEffect(()=>{
     const geo = navigator.geolocation;
-    if (!geo) {
-      setError('Geolocation is not supported');
+    if(!geo){
+      setError("Hey this isnt working");
       return;
     }
-    let watcher = geo.watchPosition(onChange, onError);
-    return () => geo.clearWatch(watcher);
-  }, []);
 
+    let watcher = geo.watchPosition(onChange, onError);
+    return ()=> geo.clearWatch(watcher);
+  },[]);
+
+  const hitBox={
+    height:"100px",
+    width:"100px",
+    backgroundColor:color
+  };
 
   return (
-      <div className="App">
+    <div className="App">
+      <h1>{position.x+" "+position.y+""+position.accuracy}</h1>
+      <p>{position.z===null?"No info to report": position.z}</p>
+      {error}
 
-        {position.latitude}
-        {position.longitude}
-        {error}
-
-      </div>
+      <div style={hitBox}></div>
+    </div>
   );
 }
 
